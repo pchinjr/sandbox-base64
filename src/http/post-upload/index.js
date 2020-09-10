340694e11f01513a72d1c4c69f70035ba7fdcbfc
Paul Chin Jr <pchinjr@gmail.com>
2020-09-10 19:57:27 -0400

    stringy

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,16 +1,23 @@
+let fs = require('fs')
 exports.handler = async function http(req) {
 
   let body = req.body //base64 encoded string
-  let buff = Buffer.from(body) //buffer that converts to string
+  let data = body.replace(/^data:image\/\w+;base64,/, "");
+  console.log(data)
+  let buff = Buffer.from(data, 'base64')
+  fs.writeFileSync('image.png',buff)
+  console.log('wrote file')
   let b64 = buff.toString('base64') // still the wrong one - needs to be decoded again
-  let secondBuff = Buffer.from(b64, 'base64')
-  let decoded = secondBuff.toString('utf8') //still not decoded
+
+
+  //let secondBuff = Buffer.from(b64, 'base64')
+  //let decoded = secondBuff.toString('utf8') //still not decoded
 
   return {
     statusCode: 200,
     headers: {
       'content-type': 'image/png'
     },
-    body: JSON.stringify(decoded),
+    body: JSON.stringify(b64),
   }
 }
